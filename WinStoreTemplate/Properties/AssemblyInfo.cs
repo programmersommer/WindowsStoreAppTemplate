4e38374d980bfcd26b1f610e4506a0e15433177e
Alexej Sommer <asommer@yandex.ru>
2015-07-04 09:52:36 +0300

    Assembly name changed

--- WinStoreTemplate/Properties/AssemblyInfo.cs
+++ WinStoreTemplate/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WinStoreTemplate")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Alexej Sommer Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
